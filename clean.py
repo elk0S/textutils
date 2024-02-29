b8d3d00ab9c91eb97da1724241e727a672d9de5b
Андрей Александрович Афанакин <elk@alpha.CORP.UNECON.RU>
2024-02-29 14:41:17 +0300

    Initial commit

---
+++ clean.py
@@ -0,0 +1,7 @@
+def remove_stopwords(text):
+    # ваш код для удаления стоп-слов
+    pass
+
+def clean_text(text):
+    # ваш код для очистки текста
+    pass
---
+++ utils.py
@@ -0,0 +1,7 @@
+def load_text(file_path):
+    # ваш код для загрузки текста из файла
+    pass
+
+def save_text(text, file_path):
+    # ваш код для сохранения текста в файл
+    pass
